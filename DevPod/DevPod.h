1d5e8d661af826143816833b6bc0876880223754
Vadim Smal <vvsmal@avito.ru>
2019-05-14 16:03:31 +0300

    Initial commit

---
+++ DevPod/DevPod.h
@@ -0,0 +1,11 @@
+#import <UIKit/UIKit.h>
+
+//! Project version number for DevPod.
+FOUNDATION_EXPORT double DevPodVersionNumber;
+
+//! Project version string for DevPod.
+FOUNDATION_EXPORT const unsigned char DevPodVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DevPod/PublicHeader.h>
+
+
---
+++ DevPod/DevPodViewController.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+public class DevPodViewController: UIViewController {
+    
+    override public func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor.red
+    }
+}
+
